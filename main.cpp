1c0348b519fbf651f5c9188dee2e760d80574776
Christine Susic <csusic022@insite.4cd.edu>
2026-03-01 17:39:43 -0800

    Commit

--- main.cpp
+++ main.cpp
@@ -8,31 +8,32 @@ const int W1 = 10, W2 = 8;
 
 class Movie {
 private:
-    string screenwriter, movieTitle;
+    string movieTitle, screenwriter;
     int releaseYear;
 public:
     //setter and getter functions
-    int getScreenwriter() const          {return screenwriter;}      
-    void setScreenwriter (string s)      {screenwriter = s;}
     int getMovieTitle() const            {return movieTitle;}      
     void setMovitTitle (string s)        {movieTitle = m;} 
     int getReleaseYear() const           {return releaseYear;}      
     void setReleaseYear (string s)       {releaseYear = r;} 
+    int getScreenwriter() const          {return screenwriter;}      
+    void setScreenwriter (string s)      {screenwriter = s;}
     //print function
     void print() {
         cout << left;
+        cout << "Movie: ";
+        cout << movieTitle << endl;
+        cout << setw(W1) << "Year released: ";
+        cout << setw(W2) << releaseYear << endl;
         cout << setw(W1) << "Screenwriter: ";
         cout << setw(W2) << screenwriter << endl;
-        cout << setw(W1) << "Movie Title: ";
-        cout << setw(W2) << movieTitle << endl;
-        cout << setw(W1) << "Release Year: ";
-        cout << setw(W2) << releaseYear << endl;
         cout << endl;
     }
 };
 
 int main() {
-    
+    vector<Movie> movies;
+    ifstream fin ("input.txt");
     
     return 0;
 }
