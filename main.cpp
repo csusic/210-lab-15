16ed94820e8cecde5a71e9dbe2ed2f0484dfaa80
Christine Susic <csusic022@insite.4cd.edu>
2026-03-01 17:19:54 -0800

    Commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// COMSC-210 | Lab 15 | Christine Susic
+
+#include <iostream>
+using namespace std;
+
+class Movie {
+private:
+    string screenwriter, movieTitle;
+    int releaseYear;
+public:
+    //setter and getter functions
+    int getScreenwriter() const          {return screenwriter;}      
+    void setScreenwriter (string s)      {screenwriter = s;}
+    int getMovieTitle() const            {return movieTitle;}      
+    void setMovitTitle (string s)        {movieTitle = m;} 
+    int getReleaseYear() const           {return releaseYear;}      
+    void setReleaseYear (string s)       {releaseYear = r;} 
+};
+
+int main() {
+    
+    
+    return 0;
+}
